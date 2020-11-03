d86e1502722cc48a846180eaaf910563a9ea387e
Benn Hamer <benn.hamer@orica.com>
2020-11-04 07:12:30 +1100

    Part 1.3: Course Information step3

--- part1/1_1_courseinfo/src/index.js
+++ part1/1_1_courseinfo/src/index.js
@@ -4,24 +4,30 @@ import ReactDOM from 'react-dom'
 const App = () => {
   
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
     return (
-      <h1>{props.course}</h1>
+      <h1>{props.c}</h1>
     )
   }
-  const Content = () => {
+  const Content = (props) => {
     return (
       <div>
-        <Part part = {part1} exercise={exercises1}/>
-        <Part part = {part2} exercise={exercises2}/>
-        <Part part = {part3} exercise={exercises3}/>
+        <Part part = {props.p1.name} exercise={props.p1.exercises}/>
+        <Part part = {props.p2.name} exercise={props.p2.exercises}/>
+        <Part part = {props.p3.name} exercise={props.p3.exercises}/>
       </div>
     )
   }
@@ -38,9 +44,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content/>
-      <Total ex1 = {exercises1} ex2={exercises2} ex3={exercises3}/>      
+      <Header c = {course}/>
+      <Content p1 = {part1} p2 = {part2} p3 = {part3}/>
+      <Total ex1 = {part1.exercises} ex2={part2.exercises} ex3={part3.exercises}/>      
     </div>
   )
 
